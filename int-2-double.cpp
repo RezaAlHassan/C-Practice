dbf39276bb26b9affe5951bdf990e2419c00c95f
Reza Al Hassan <rezahasan1198@gmail.com>
2023-09-05 01:48:25 +0600

    Add files via upload

---
+++ int-2-double.cpp
@@ -0,0 +1,23 @@
+
+// Online C++ Compiler - Build, Compile and Run your C++ programs online in your favorite browser
+
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+
+    double num1 = 2.1;
+    double num2 = 2.3;
+
+    int i = int(num1);
+    int j = int(num2);
+
+    int sum = 0;
+    sum = i + j;
+    // outputs 4
+    cout << sum << endl;
+    return 0;
+
+}
